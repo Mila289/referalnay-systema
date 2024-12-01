f0ba0e04931a48e0853994e7ac24ba352bbdf545
Mila289 <mila.krasulina@gmail.com>
2024-12-01 20:27:34 +0700

    В основном urls.py для документации API добавляю drf-yasg путем установки в консоли и добавляю саму документацию.

--- PythonProject1/PythonProject1/urls.py
+++ PythonProject1/PythonProject1/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +29,23 @@ urlpatterns = [
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Referral API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="support@yourdomain.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
