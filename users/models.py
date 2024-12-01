cca22bf0be0b2436f10c6e998886cfed5ba7e568
Mila289 <mila.krasulina@gmail.com>
2024-12-01 19:30:49 +0700

    Создание модели для пользователя и инвайт-кода.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
+# models.py
 from django.db import models
+import random
+import string
 
-# Create your models here.
+
+class User(models.Model):
+    phone_number = models.CharField(max_length=20, unique=True)
+    auth_code = models.CharField(max_length=4, blank=True, null=True)
+    invite_code = models.CharField(max_length=50, blank=True, null=True)
+
+    def generate_auth_code(self):
+        return ''.join(random.choices(string.digits, k=4))
+
+    def generate_invite_code(self):
+        return ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+
+    def __str__(self):
+        return f"User(phone={self.phone_number})"
+
+
